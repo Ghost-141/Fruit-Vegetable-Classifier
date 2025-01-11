06170222d8718f27ef53a8f282c26739e7483d73
Imtiaz Ahammed <117445651+Ghost-141@users.noreply.github.com>
2025-01-12 02:18:22 +0600

    First Update
    Updated the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Fruit-Vegetable-Classifier
- Classify between fruit or vegetable across 36 classes using custom cnn model. Along with gradio app interfacce
+A Deep Learning model designed to classify images of fruits and vegetables into their respective categories. This repository contains a pre-trained model, a Jupyter notebook demonstrating the training process, and a Gradio-powered web interface for testing the classifier.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Usage](#usage)
+- [Features](#features)
+- [Installation](#installation)
+- [Model Info](#model-training)
+- [Web Interface](#web-interface)
+- [Conclusion](#conclusion)
+
+## Introduction
+This projects utilizes the power of CNN (Convolutional Neural Network) to Classify between fruit and vegetable across 36 different classes. 
+([Dataset](https://www.kaggle.com/kritikseth/fruit-and-vegetable-image-recognition)) 
+
+## Usage
+- Download the folder and enjoy
+- Ensure that the pre-trained [`custom_cnn_model.h5`] file is in the project directory
+- Run the [`classify.py`] file to launch the web-interface and enjoy the app
+   
+## Features
+- Custom pre-trained model for quick use on classification task
+- Jupyter notebook to demonstrate the whole process of model training and testing
+- Gradio web interface for easy testing and interaction   
+
+## Model Info
+- Achieved an accuracy of 93% on test dataset
+- Model was trained for 40 epochs
+- Model training was stopped earlier to stop overfitting
+
+## Web Interface
+- Used the gradio to make a simple web interface along with interaction
+
+## Conclusion
+- Having any issue feel free to reach out
+- Please give it a star if you find it useful
+
+
---
+++ classify.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import gradio as gr
+
+# Load your model
+model = tf.keras.models.load_model("custom_cnn_model.h5")
+
+# Class Names
+class_map = [
+    'apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower',
+    'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi',
+    'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple',
+    'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato',
+    'turnip', 'watermelon']
+
+def prediction(test_image):
+    """Make a prediction using the loaded model and show the result."""
+ 
+    # Load and preprocess the image
+    test_img = image.load_img(test_image, target_size=(192, 192)) 
+    test_img_array = image.img_to_array(test_img)
+    test_img_batch = tf.expand_dims(test_img_array, 0)
+
+    #Make Prediction
+    predictions = model.predict(test_img_batch)
+    prediction = model.predict(test_img_batch)
+    score = tf.nn.softmax(prediction[0])
+    predicted_class = class_map[np.argmax(score)]
+    confidence = np.max(score)*100
+    return f"Predicted Class: {predicted_class}, Confidence: {confidence:.2f}%"
+
+sample_images= ['Apple.jpg','corn.jpg','potato1.jpeg','Tomato.jpg','Banana.jpg']
+
+#Gradio interface
+interface = gr.Interface(
+    fn=prediction, 
+    inputs=gr.Image(type="filepath"),
+    outputs="text", 
+    title="Vegetable & Fruit Classifer",
+    examples=sample_images,
+    description="Upload image of any vegetable or furit to classify.\n\n ![Vegetable GIF](https://media.giphy.com/media/LrY6OiICHS1TWZpGkf/giphy.gif)"
+)
+interface.launch()
